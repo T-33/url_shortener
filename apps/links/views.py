f50cee0aa77ee9f73c7043577f6e811999c8a033
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-09-15 14:50:21 +0600

    feat: add redirect view and click registration - add link redirect view that handles user's redirect and click creation - add link redirect view url - add click viewset - add click urls

--- apps/links/views.py
+++ apps/links/views.py
@@ -1,12 +1,24 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Link
+from .models import Link, Click
 from .serializers import LinkSerializer
 
 User = get_user_model()
 
+def link_redirect_view(request, short_code: str):
+
+    link = get_object_or_404(Link, short_code=short_code)
+
+    Click.objects.create(
+        link=link,
+        ip_address=request.META.get('REMOTE_ADDR'),
+        user_agent=request.META.get('HTTP_USER_AGENT', '')
+    )
+
+    return redirect(link.original_url)
 class LinkViewset(ModelViewSet):
     queryset = Link.objects.all()
     serializer_class = LinkSerializer
@@ -24,3 +36,7 @@ class LinkViewset(ModelViewSet):
             return Link.objects.all()
         else:
             return Link.objects.filter(owner=user)
+
+class ClickViewset(ModelViewSet):
+    queryset = Click.objects.all()
+    permission_classes = [IsAuthenticated]
--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from apps.links.views import link_redirect_view
 urlpatterns = [
+    path('<str:short_code>/', link_redirect_view, name='redirect'),
     path('admin/', admin.site.urls),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
